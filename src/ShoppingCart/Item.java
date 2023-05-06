65b6f8b6e1fad40c071edcad67e6c10087fdef09
Salmaabdelaziz271 <93342442+Salmaabdelaziz271@users.noreply.github.com>
2023-05-06 19:08:58 +0300

    add Item class

---
+++ src/ShoppingCart/Item.java
@@ -0,0 +1,44 @@
+package ShoppingCart;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Item {
+    Item(int itemID,String itemName, double itemPrice, int itemAvailableAmount, double itemDiscount){
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemAvailableAmount = itemAvailableAmount;
+        this.itemDiscount = itemDiscount;
+    }
+    private int itemID;
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    private String itemName;
+    public String getItemName() {
+        return itemName;
+    }
+
+    private double itemPrice;
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    private int itemAvailableAmount;
+    public int getItemAvailableAmount() {
+        return itemAvailableAmount;
+    }
+    public void setItemAvailableAmount(int itemAvailableAmount) {
+        this.itemAvailableAmount = itemAvailableAmount;
+    }
+    private double itemDiscount;
+    public double getItemDiscount() {
+        return itemDiscount;
+    }
+
+}
