1c25fe831af6f3a5bd192a06fbc69f6cd35d8fc0
Salmaabdelaziz271 <93342442+Salmaabdelaziz271@users.noreply.github.com>
2023-05-06 19:07:04 +0300

    Update ShoppingCart.java

--- src/ShoppingCart/ShoppingCart.java
+++ src/ShoppingCart/ShoppingCart.java
@@ -1,63 +1,100 @@
 package ShoppingCart;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ShoppingCart{
+public class ShoppingCart {
 
-    public ShoppingCart(int ShoppingCartID, int userID){
-        this.ShoppingCartID = ShoppingCartID;
+    public ShoppingCart(int userID) {
         this.userID = userID;
         numberOfItems = 0;
     }
-    public int ShoppingCartID;
-    public int getShoppingCartID() {
-        return ShoppingCartID;
-    }
+    // private int ShoppingCartID;
+    //public int getShoppingCartID() {
+    //return ShoppingCartID;
+    //}
 
-    public int userID;
+    private int userID;
+
     public int getUserID() {
         return userID;
     }
 
-    public int numberOfItems;
+    private int numberOfItems;
+
     public int getNumberOfItems() {
         return numberOfItems;
     }
-    public ArrayList<cartItem> items= new ArrayList<>();
 
-    public void addItem(cartItem item){
-        //implement
-    }
+    private ArrayList<CartItem> items = new ArrayList<>();
 
-
-    
-    private File itemsFile = new File("items.txt");
-    public ArrayList<cartItem> viewCatalog(){
-        // implement
-        
-        if(itemsFile.length()==0){
-            return items;
-        }
-
-        
+    public ArrayList<CartItem> getItems() {
         return items;
-    
     }
-    
-//    public void removeItem(cartItem item){
-//
-//    }
+
+    public double getTotalPrice() {
+        double totalPrice = 0;
+        for (int i = 0; i < items.size(); i++) {
+            totalPrice += (items.get(i).getItemPrice() - items.get(i).getItemDiscount()) * items.get(i).getCartItemQuantity();
+        }
+        return totalPrice;
+    }
+    public CartItem searchItem(int itemID, int quantity) {
+        FileItem itemFile = new FileItem();
+        ArrayList<Item> itemsArray = itemFile.load();
+        for (int i = 0; i < itemsArray.size(); i++) {
+            if (itemsArray.get(i).getItemID() == itemID) {
+                if (itemsArray.get(i).getItemAvailableAmount() >= quantity) {
+                    int itemAvailableAmount = itemsArray.get(i).getItemAvailableAmount() - quantity;
+                    itemsArray.get(i).setItemAvailableAmount(itemAvailableAmount);
+                    itemFile.saveAmount(itemsArray);
+                    CartItem cartItem = new CartItem(itemsArray.get(i).getItemID(), itemsArray.get(i).getItemName(), itemsArray.get(i).getItemPrice(), itemsArray.get(i).getItemAvailableAmount(), itemsArray.get(i).getItemDiscount(), quantity);
+                    return cartItem;
+                }
+                else {
+                    System.out.println("Sorry, the quantity you entered is not available");
+                    return null;
+                }
+            }
+        }
+        System.out.println("Sorry, the item you entered is not available");
+        return null;
+    }
+
+    public void addItem(CartItem item) {
+        if (item != null) {
+            for(int i = 0; i < items.size(); i++){
+                if(items.get(i).getItemID() == item.getItemID()){
+                   System.out.println("This item is already in your cart\n Do you want to increase the quantity of this item? (y/n)");
+                     Scanner input = new Scanner(System.in);
+                     char choice = input.next().charAt(0);
+                        if(choice == 'y'){
+                            items.get(i).setCartItemQuantity(items.get(i).getCartItemQuantity() + item.getCartItemQuantity());
+                            FileShoppingCart shoppingCartFile = new FileShoppingCart();
+                            shoppingCartFile.save(this);
+                            return;
+                        }
+                        else{
+                            return;
+                        }
+                }
+            }
+            items.add(item);
+            numberOfItems++;
+            FileShoppingCart shoppingCartFile = new FileShoppingCart();
+            shoppingCartFile.save(this);
+        }
+    }
 
 
-//    public void viewShoppingCart(){
-//
-//    }
+    public ArrayList<Item> viewCatalog() {
+        FileItem itemFile = new FileItem();
+        ArrayList<Item> items = itemFile.load();
+        for (int i = 0; i < items.size(); i++) {
+            System.out.println("itemID: " + items.get(i).getItemID() + "\nitemName: " + items.get(i).getItemName() + "\nitemPrice: $" + items.get(i).getItemPrice() + "\nitemAvailableAmount: " + items.get(i).getItemAvailableAmount() + " pieces" + "\nitemDiscount: $" + items.get(i).getItemDiscount());
+            System.out.println("----------------------------------------------------------------------------------------------------------------------------");
+        }
+        return items;
+    }
 
 }
